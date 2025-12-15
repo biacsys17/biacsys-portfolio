4c2ec36540926973bf6b9856236ac1aa991eed5a
biacsys17 <biacsys74@gmail.com>
2025-12-15 22:53:34 +0530

    Update contact_form.tsx

--- frontend/src/Pages/contact_form.tsx
+++ frontend/src/Pages/contact_form.tsx
@@ -31,7 +31,7 @@ export default function ContactForm() {
 
   // Send data to backend
   async function submitToServer(payload: FormState) {
-  const res = await fetch("https://biacsys-backend-env.eba-mmrdihu8.ap-southeast-2.elasticbeanstalk.com/api/contact", {
+  const res = await fetch("https://api.biacsys.com/api/contact", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
