27a6d507dd112c4b565821099f702dcae062a67f
Rohith JS <96683209+jsrohith07@users.noreply.github.com>
2026-03-26 20:44:29 +0530

    Update styled.div.tsx

--- frontend/src/Components/Ui/styled.div.tsx
+++ frontend/src/Components/Ui/styled.div.tsx
@@ -3,7 +3,7 @@ const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.backgroundColor};
   color: ${({ theme }) => theme.textColor};
   padding: 0px;
-  min-height: 0;
+  min-height: 10vh;
   overflow-x: hidden;
   
 `;
